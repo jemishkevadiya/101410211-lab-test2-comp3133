511c98771a133a4470795ad7df63d78710a3bcf0
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-04-02 17:18:35 -0400

    Changes in server.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { MissionlistComponent } from './missionlist/missionlist.component';
-import { MissionfilterComponent } from './missionfilter/missionfilter.component';
-import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 
-export const routes: Routes = [
-  { path: '', component: MissionlistComponent },
-  { path: 'filter', component: MissionfilterComponent },
-  { path: 'mission/:id', component: MissiondetailsComponent }
-];
+export const routes: Routes = [];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { MissionlistComponent } from './app/missionlist/missionlist.component';
+import { MissiondetailsComponent } from './app/missiondetails/missiondetails.component';
+import { MissionfilterComponent } from './app/missionfilter/missionfilter.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes = [
+  { path: '', component: MissionlistComponent },
+  { path: 'filter', component: MissionfilterComponent },
+  { path: 'mission/:id', component: MissiondetailsComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
